4dea37ef6e8df1b9b0a1733c0ee4709b4898b7fa
Adarsh Hatkar <80952515+AdarshHatkar@users.noreply.github.com>
2025-07-21 17:37:43 +0530

    added number utils

--- src/universal/number/index.ts
+++ src/universal/number/index.ts
@@ -35,3 +35,43 @@ export function inRange(number: number, lower: number, upper: number): boolean {
   
   return number >= lower && number <= upper;
 }
+
+
+export const convertToInt = (data: any): number => {
+    if (isNaN(data)) {
+        throw new Error('Invalid input: not a number');
+    }
+    if (data === null || data === undefined) {
+        throw new Error('Invalid input: null or undefined');
+    }
+
+    if (typeof data === 'number') {
+        data = data + '';
+    }
+
+    return +parseInt(data);
+};
+
+export const convertToTwoDecimalInt = (data: any): number => {
+    if (isNaN(data)) {
+        throw new Error('Invalid input: not a number');
+    }
+    if (data === null || data === undefined) {
+        throw new Error('Invalid input: null or undefined');
+    }
+
+    if (typeof data === 'number') {
+        data = data + '';
+    }
+    return +parseFloat(data).toFixed(2);
+};
+
+export const randomNumberWithFixedLength = (length: number): number => {
+    if (length <= 0 || !Number.isInteger(length)) {
+        throw new Error('Length must be a positive integer.');
+    }
+    return Math.floor(
+        Math.pow(10, length - 1) +
+        Math.random() * (9 * Math.pow(10, length - 1))
+    );
+};
